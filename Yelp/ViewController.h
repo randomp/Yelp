f8b499c2e19852ba7ce1f25d487d19a27f7778a6
peiqi <peiqi@yahoo-inc.com>
2014-09-17 21:19:03 -0700

    Initial Commit

---
+++ Yelp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Yelp
+//
+//  Created by Peiqi Zheng on 9/17/14.
+//  Copyright (c) 2014 Peiqi Zheng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Yelp/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Yelp
+//
+//  Created by Peiqi Zheng on 9/17/14.
+//  Copyright (c) 2014 Peiqi Zheng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
